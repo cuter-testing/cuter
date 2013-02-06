32eadff16e0fa4df8c62bca1d0c45cbdfad90f32
Aggelos Giantsios <aggelgian@gmail.com>
2013-02-06 22:18:06 +0200

    Tweaking EUnit timeout + fix spec

--- utest/coordinator_tests.erl
+++ utest/coordinator_tests.erl
@@ -5,13 +5,11 @@
 -spec test() -> 'ok' | {'error' | term()}.
 
 -spec toy_test_() -> term().
-
 toy_test_() ->
   Setup =
     fun() ->
       Version = 'short',
-      Cores = erlang:system_info(schedulers_online),
-      Conf = [{'number_of_cores', Cores}],
+      Conf = [{'number_of_cores', 2}],
       Toys = [bang, genstress, big, ehb, ets_test, parallel, pcmark, serialmsg, timer_wheel],
       X = [{T, T:bench_args(Version, Conf)} || T <- Toys],
       [{T, A} || {T, L} <- X, A <- L]
@@ -21,6 +19,9 @@ toy_test_() ->
       R = coordinator:run(Toy, run, [Arg, 'foo', 'bar']),
       ?assertEqual({'ok', {ok, ok}}, R)
     end,
-  Inst = fun(L) -> [fun() -> Test(A) end || A <- L] end,
+  Inst = 
+    fun(L) ->
+      [{timeout, 10, {atom_to_list(T), fun() -> Test({T, A}) end}} || {T, A} <- L]
+    end,
   {foreach, Setup, [Inst]}.
 
