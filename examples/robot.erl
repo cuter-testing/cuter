e34b5dce0162d49a3c6077e1b4c98979e38733ee
Aggelos Giantsios <aggelgian@gmail.com>
2013-12-23 01:09:52 +0200

    Add example code for testing

---
+++ examples/robot.erl
@@ -0,0 +1,97 @@
+-module(robot).
+-export([solve/4]).
+
+%%
+%% Simple DFS imple
+%%
+%% Assume a NxN map and a robot that wants to move from Start to End.
+%% The robot can move Up, Right, Down and Left.
+%% It cannot move to a cell that is occupied by an Obstacle.
+%%
+%% solve/4 returns the length of a feasible path.
+%% If no path is available, it returns false.
+%%
+%% BUG: An exeption is raised when the robot tries to move to a point 
+%% that is outside the map on the left side.
+%% e.g. robot:solve(7, {7,4}, {1,2}, [{1,4},{5,7},{7,6}]).
+%%
+%%
+%% Sample Usage:
+%% robot:solve(8, {6,7}, {7,3}, [{5,5},{6,5},{7,5}]). (Result is 3)
+%% 
+%% Sample Cuter Usage
+%% erl -noinput -pa ebin -pa testsuite/ebin -eval "coordinator:run(robot, solve, [8, {6,7}, {7,3}, [{5,5},{6,5},{7,5}]], 30)" -s init stop
+
+-define(MAP_SIZE, 8).
+
+-type map_size() :: 1..?MAP_SIZE.
+-type coordinate() :: 1..?MAP_SIZE.
+-type point() :: {coordinate(), coordinate()}.
+-type obstacles() :: [point()].
+
+-spec solve(map_size(), point(), point(), obstacles()) -> term().
+%solve(N, {SX, SY}, {EX, EY}, _) when SX > N; SY > N; EX > N; EY > N -> false;
+%solve(N, Start, End, Obstacles) -> dfs(N, Start, End, Obstacles, [], [], []).
+solve(N, Start, End, Obstacles) ->
+  case validate_endpoints(N, Start, End) andalso validate_obstacles(N, Obstacles) of
+    false -> false;
+    true -> dfs(N, Start, End, Obstacles, [], [], [])
+  end.
+
+validate_endpoints(N, {SX, SY}, {EX, EY}) when SX > N; SY > N; EX > N; EY > N -> false;
+validate_endpoints(_, _, _) -> true.
+
+validate_obstacles(_, []) -> true;
+validate_obstacles(N, [{X, Y}|_]) when X > N; Y > N -> false;
+validate_obstacles(N, [_|Os]) -> validate_obstacles(N, Os).
+
+-spec dfs(map_size(), point(), point(), obstacles(), [point()], [point()], [point()]) -> integer() | false.
+dfs(_N, _End, _End, _Obstacles, _Queue, _Visited, Path) ->
+  length(Path);
+dfs(N, Curr, End, Obstacles, Queue, Visited, Path) ->
+  case expand(N, Curr, Obstacles, Queue, Visited) of
+    [] -> false;
+    [M | Ms] ->
+      _ = validate_move(M, N),
+      dfs(N, M, End, Obstacles, Ms, [M|Visited], [M|Path])
+  end.
+
+validate_move({X, Y}, N) when X > 0, Y > 0, X =< N, Y =< N ->
+  ok;
+validate_move(Point, _N) -> exit({invalid_point, Point}).
+
+-spec expand(map_size(), point(), obstacles(), [point()], [point()]) -> {[point()], [point()]}.
+expand(N, {X, Y}, Obstacles, Pending, Visited) -> 
+  Up = {X, Y+1},
+  Right = {X+1, Y},
+  Down = {X, Y-1},
+  Left = {X-1, Y},
+  L = lists:foldl(
+    fun(M, Acc) ->
+      case can_move(N, M, Obstacles) andalso (not is_visited(M, Visited)) of
+        true -> [M|Acc];
+        false -> Acc
+      end
+    end,
+    Pending,
+    [Left, Down, Right, Up]
+  ),
+  L.
+
+-spec is_visited(point(), [point()]) -> boolean().
+is_visited(Point, Visited) -> member(Point, Visited).
+
+-spec can_move(map_size(), point(), obstacles()) -> boolean().
+can_move(N, {X, Y}, _) when X > N; Y > N -> false;
+can_move(_, {_, Y}, _) when Y < 1 -> false;
+can_move(_, _Point, [_Point|_]) -> false;
+can_move(_, _, []) -> true;
+can_move(_, Point, Obstacles) -> not member(Point, Obstacles).
+
+
+%% An implementation of lists:member/2 that is not a BIF
+%member(X, L) -> lists:member(X, L);
+member(_, []) -> false;
+member(X, [X|_]) -> true;
+member(X, [_|L]) -> member(X, L).
+
