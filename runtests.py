60ef77380fa4940d419f3f3ebdbfb9b088975c3d
Aggelos Giantsios <aggelgian@gmail.com>
2014-11-27 10:54:55 +0200

    Add meck ebin to the configure script

--- runtests.py
+++ runtests.py
@@ -3,12 +3,21 @@
 
 import sys, os
 
+def run(mod, ebins):
+  pas = " ".join(["-pa %s" % e for e in ebins])
+  print "Testing %s..." % mod
+  os.system("erl -noshell %s -eval \"eunit:test(%s, [verbose])\" -s init stop" % (pas, mod))
+
 if len(sys.argv) < 2:
   print "Too few arguments..."
   sys.exit(0)
 
 ebin = sys.argv[1]
-tests = ["cuter", "cuter_codeserver", "cuter_cerl", "cuter_monitor", "cuter_iserver", "cuter_eval", "cuter_json", "cuter_solver"]
+meck = sys.argv[2] if len(sys.argv) >= 3 else None
+tests = ["cuter", "cuter_cerl", "cuter_iserver", "cuter_eval", "cuter_json", "cuter_solver"]
+tests_with_meck = ["cuter_codeserver", "cuter_monitor"]
 for t in tests:
-  print "Testing %s..." % t
-  os.system("erl -noshell -pa %s -pa ../meck/ebin -eval \"eunit:test(%s, [verbose])\" -s init stop" % (ebin, t))
+  run(t, [ebin])
+if meck:
+  for t in tests_with_meck:
+    run(t, [ebin, meck])
