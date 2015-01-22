4b517f17d545d9d76a5f04da9a8ac88feeebd4f3
Aggelos Giantsios <aggelgian@gmail.com>
2015-01-23 00:07:36 +0200

    Add cuter_erlang tests

---
+++ utest/cuter_erlang_tests.erl
@@ -0,0 +1,54 @@
+%% -*- erlang-indent-level: 2 -*-
+%%------------------------------------------------------------------------------
+-module(cuter_erlang_tests).
+
+-include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include("include/eunit_config.hrl").
+
+-spec test() -> ok | {error | term()}. %% Silence dialyzer warning
+
+%% Test cuter_erlang:atom_to_list/1.
+-spec atom_to_list_test() -> boolean().
+atom_to_list_test() ->
+  proper:quickcheck(prop_atom_to_list(), [{to_file, user}, {numtests, 1000}]).
+
+-spec prop_atom_to_list() -> proper:outer_test().
+prop_atom_to_list() ->
+  ?FORALL(X, atom(), erlang:atom_to_list(X) =:= cuter_erlang:atom_to_list(X)).
+
+%% Test cuter_erlang:'<'/2.
+-spec lt_test() -> boolean().
+lt_test() ->
+  proper:quickcheck(prop_lt(), [{to_file, user}, {numtests, 1000}]).
+
+-spec prop_lt() -> proper:outer_test().
+prop_lt() ->
+  ?FORALL({X,Y}, {any(),any()}, (X < Y) =:= cuter_erlang:'<'(X, Y)).
+
+%% Test cuter_erlang:'=<'/2.
+-spec lteq_test() -> boolean().
+lteq_test() ->
+  proper:quickcheck(prop_lteq(), [{to_file, user}, {numtests, 1000}]).
+
+-spec prop_lteq() -> proper:outer_test().
+prop_lteq() ->
+  ?FORALL({X,Y}, {any(),any()}, (X =< Y) =:= cuter_erlang:'=<'(X, Y)).
+
+%% Test cuter_erlang:'>'/2.
+-spec gt_test() -> boolean().
+gt_test() ->
+  proper:quickcheck(prop_gt(), [{to_file, user}, {numtests, 1000}]).
+
+-spec prop_gt() -> proper:outer_test().
+prop_gt() ->
+  ?FORALL({X,Y}, {any(),any()}, (X > Y) =:= cuter_erlang:'>'(X, Y)).
+
+%% Test cuter_erlang:'>='/2.
+-spec gteq_test() -> boolean().
+gteq_test() ->
+  proper:quickcheck(prop_gteq(), [{to_file, user}, {numtests, 1000}]).
+
+-spec prop_gteq() -> proper:outer_test().
+prop_gteq() ->
+  ?FORALL({X,Y}, {any(),any()}, (X >= Y) =:= cuter_erlang:'>='(X, Y)).
