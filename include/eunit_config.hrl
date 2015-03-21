4fc27fc7fdcd17fdd11c4172c6c1a6eb9ca1af14
Aggelos Giantsios <aggelgian@gmail.com>
2015-03-21 21:38:19 +0200

    Fix for currently unsupported maps

--- include/eunit_config.hrl
+++ include/eunit_config.hrl
@@ -5,6 +5,7 @@
 %% Various
 %%====================================================================
 
--define(MODS_LIST, [lists, dict, orddict, ets, os, string, cerl, filelib, beam_lib]).
+%-define(MODS_LIST, [lists, dict, orddict, ets, os, string, cerl, filelib, beam_lib]).
+-define(MODS_LIST, [lists, dict, orddict, ets, os, string, filelib, beam_lib]).
 -define(TRACE_DEPTH, 100).
 -define(EXEC_PREFIX, "test").
