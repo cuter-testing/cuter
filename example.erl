3cfe53aa4ced140b35e5e03995b50efe0e79a03d
Nikolaos S. Papaspyrou <nickie@softlab.ntua.gr>
2015-04-02 18:33:54 +0300

    Add a few variants of the running example

--- example.erl
+++ example.erl
@@ -3,7 +3,8 @@
 
 -module(example).
 -export([foo11/1, foo21/1, foo12/1, foo22/1,
-         foo11s/1, foo21s/1, foo12s/1, foo22s/1]).
+         foo11s/1, foo21s/1, foo12s/1, foo22s/1,
+         foo11i/1, foo21i/1, foo12i/1, foo22i/1]).
 
 foo11(L) ->
   lists:foreach(fun fcmp1/1, L).
@@ -17,21 +18,37 @@ foo12(L) ->
 foo22(L) when length(L) < 3 -> small;
 foo22(L) -> lists:foreach(fun fcmp2/1, L).
 
--spec foo11s(list(any())) -> any().
+-spec foo11s([any()]) -> any().
 foo11s(L) ->
   lists:foreach(fun fcmp1/1, L).
 
--spec foo21s(list(any())) -> any().
+-spec foo21s([any()]) -> any().
 foo21s(L) when length(L) < 3 -> small;
 foo21s(L) -> lists:foreach(fun fcmp1/1, L).
 
--spec foo12s(list(any())) -> any().
+-spec foo12s([any()]) -> any().
 foo12s(L) ->
   lists:foreach(fun fcmp2/1, L).
 
--spec foo22s(list(any())) -> any().
+-spec foo22s([any()]) -> any().
 foo22s(L) when length(L) < 3 -> small;
 foo22s(L) -> lists:foreach(fun fcmp2/1, L).
+
+-spec foo11i([integer()]) -> ok.
+foo11i(L) ->
+  lists:foreach(fun fcmp1/1, L).
+
+-spec foo21i([integer()]) -> small | ok.
+foo21i(L) when length(L) < 3 -> small;
+foo21i(L) -> lists:foreach(fun fcmp1/1, L).
+
+-spec foo12i([integer()]) -> ok.
+foo12i(L) ->
+  lists:foreach(fun fcmp2/1, L).
+
+-spec foo22i([integer()]) -> small | ok.
+foo22i(L) when length(L) < 3 -> small;
+foo22i(L) -> lists:foreach(fun fcmp2/1, L).
 
 fcmp1(X) ->
   case cmp(X) of
---
+++ example.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+EXAMPLES="11 21 12 22 11s 21s 12s 22s 11i 21i 12i 22i"
+
+if [ "$*" != "" ]; then
+  EXAMPLES="$*"
+fi
+
+for v in $EXAMPLES; do
+  echo "Running: $v"
+  gtime \
+  erl -noshell -pa $PWD/ebin -eval "cuter:run(example, foo$v, [[17]], 25)" \
+      -s init stop > LOG.$v
+done
