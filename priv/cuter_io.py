b1c7c8890f5fb44cf23eac3e69d14b621cbd45e2
Aggelos Giantsios <aggelgian@gmail.com>
2015-08-22 20:59:36 +0300

    Add unit tests for cuter_io.py

--- priv/cuter_io.py
+++ priv/cuter_io.py
@@ -76,3 +76,76 @@ class JsonReader:
   
   def __del__(self):
     self.fd.close()
+
+###############################################################################
+# Unit Tests
+###############################################################################
+
+def sample_entries():
+  return [
+    {
+      "kind": cc.NOT_CONSTRAINT,
+      "opcode": cc.OP_PARAMS,
+      "tag": 0,
+      "data": {u'a': [{u's': u'0.0.0.44'}, {u's': u'0.0.0.45'}, {u's': u'0.0.0.46'}], u'c': 1}
+    },
+    {
+      "kind": cc.CONSTRAINT_FALSE,
+      "opcode": cc.OP_MATCH_EQUAL_FALSE,
+      "tag": 50,
+      "data": {u'a': [{u't': 3, u'v': [110, 101, 116]}, {u's': u'0.0.0.44'}], u'c': 6}
+    },
+    {
+      "kind": cc.CONSTRAINT_TRUE,
+      "opcode": cc.OP_MATCH_EQUAL_TRUE,
+      "tag": 105,
+      "data": {u'a': [{u't': 3, u'v': [101, 114, 108, 97, 110, 103]}, {u's': u'0.0.0.44'}], u'c': 5}
+    },
+    {
+      "kind": cc.CONSTRAINT_FALSE,
+      "opcode": cc.OP_MATCH_EQUAL_FALSE,
+      "tag": 3344,
+      "data": {u'a': [{u't': 3, u'v': [116, 114, 117, 101]}, {u's': u'0.0.0.39316'}], u'c': 6}
+    }
+  ]
+
+def write_bytes(fd, bs):
+  for b in bs:
+    write_byte(fd, b)
+
+def write_byte(fd, b):
+  fd.write(struct.pack('B', b))
+
+def integer_to_i32(num):
+  return [num >> i & 0xff for i in (24, 16, 8, 0)]
+
+def test_reader():
+  fname = "tmp"
+  es = sample_entries()
+  try:
+    # Prepare the sample file.
+    fd = gzip.open(fname, "wb")
+    for e in es:
+      write_byte(fd, e["kind"])
+      write_byte(fd, e["opcode"])
+      write_bytes(fd, integer_to_i32(e["tag"]))
+      data = json.dumps(e["data"])
+      write_bytes(fd, integer_to_i32(len(data)))
+      fd.write(data)
+    fd.close()
+
+    # Test the JsonReader.
+    cglb.init()
+    i = 0
+    for tp, tag, data, _ in JsonReader(fname, 10000):
+      e = es[i]
+      assert tp == e["opcode"], "Opcode of entry {} is {} instead of {}".format(i, tp, e["opcode"])
+      assert tag == e["tag"], "Tag of entry {} is {} instead of {}".format(i, tag, e["tag"])
+      assert data == e["data"], "Data of entry {} is {} instead of {}".format(i, data, e["data"])
+      i += 1
+  finally:
+    os.remove(fname)
+
+if __name__ == '__main__':
+  import os
+  test_reader()
