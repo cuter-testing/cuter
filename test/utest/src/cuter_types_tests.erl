17fd7034aeec1cce9cfbd87c9ba81d39a3e0b450
Aggelos Giantsios <aggelgian@gmail.com>
2016-01-29 03:31:39 +0200

    Run unit tests with scripts instead from within the Makefile

---
+++ test/utest/src/cuter_types_tests.erl
@@ -0,0 +1,7 @@
+%% -*- erlang-indent-level: 2 -*-
+%%------------------------------------------------------------------------------
+-module(cuter_types_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-spec test() -> ok | {error | term()}. %% Silence dialyzer warning
