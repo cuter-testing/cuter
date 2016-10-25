b3f4972d32cf1e88463755f73684f50d72b8d424
constracti <constracti@gmail.com>
2016-10-26 01:30:14 +0300

    export smt with constraints to file
    The set of constraints is described in SMT-LIB and printed to a file output.smt in current directory.
    Output file is rewritten each time a constraint set is forwarded to the solver.
    The SMT-LIB built-in type Bool is an alternative option for a Term. Atoms atomTrue and atomFalse are removed.
    Currently only function g from module collection is supported.

--- priv/cuter_smt.py
+++ priv/cuter_smt.py
@@ -4,106 +4,196 @@ import cuter_generic_solver as cgs
 import cuter_common as cc
 import cuter_logger as clg
 
+clog = open("output.smt", "w")
+def log(msg = ""):
+	clog.write(msg + "\n")
+
+def smt(obj):
+	if type(obj) is str:
+		return obj
+	else:
+		return "(" + " ".join(map(smt, obj)) + ")"
+
 class ErlangSMT(cgs.AbstractErlangSolver):
-    def __init__(self):
-        self.model = None
-        self.params = []
-        self.axs = []
+	def __init__(self):
+		self.model = None
+		self.params = []
+		self.axioms = []
+		self.symbols = []
 
-    # =========================================================================
-    # Public API.
-    # =========================================================================
+	# =========================================================================
+	# Public API.
+	# =========================================================================
 
-    def fix_parameter(self, p, v):
-        """
-        Fixes a symbolic variable to a specific value.
-        """
-        self.axs.append("(assert (= {} {}))".format(p, v))
+	def fix_parameter(self, p, v):
+		"""
+		Fixes a symbolic variable to a specific value.
+		"""
+		pass
 
-    def add_axioms(self):
-        """
-        Adds the axioms from memory to the solver.
-        """
-        pass
+	def add_axioms(self):
+		"""
+		Adds the axioms from memory to the solver.
+		"""
+		dd = [
+			"declare-datatypes", [],
+			[
+				[
+					"Term",
+					["bool", ["bval", "Bool"]],
+					["int", ["ival", "Int"]],
+					["real", ["rval", "Real"]],
+					["list", ["lval", "TList"]],
+				],
+				[
+					"TList",
+					["nil"],
+					["cons", ["hd", "Term"], ["tl", "TList"]],
+				],
+			],
+		]
+		log(smt(dd))
+		for symbol in self.symbols:
+			log(smt(["declare-const", "|{}|".format(symbol), "Term"]))
+		for axiom in self.axioms:
+			log(axiom)
+		log(smt(["check-sat"]))
+		log(smt(["get-model"]))
 
-    def solve(self):
-        """
-        Solves a constraint set and returns the result.
-        """
-        return cc.SOLVER_STATUS_SAT
+	def solve(self):
+		"""
+		Solves a constraint set and returns the result.
+		"""
+		return cc.SOLVER_STATUS_SAT
 
-    def reset_solver(self):
-        """
-        Resets the solver.
-        """
-        pass
+	def reset_solver(self):
+		"""
+		Resets the solver.
+		"""
+		pass
 
-    def encode_model(self):
-        """
-        Encodes the resulting model to JSON.
-        """
-        return [({"s": p}, {"t": cc.JSON_TYPE_INT, "v": 1}) for p in self.params]
+	def encode_model(self):
+		"""
+		Encodes the resulting model to JSON.
+		"""
+		return [({"s": p}, {"t": cc.JSON_TYPE_INT, "v": 1}) for p in self.params]
 
-    # =========================================================================
-    # Private Methods.
-    # =========================================================================
+	# =========================================================================
+	# Private Methods.
+	# =========================================================================
 
-    # -------------------------------------------------------------------------
-    # Parse internal commands.
-    # -------------------------------------------------------------------------
+	def decode(self, data):
+		"""
+		Decodes a JSON term to its SMT representation
+		"""
+		if "s" in data:
+			if data["s"] not in self.symbols:
+				self.symbols.append(data["s"])
+			return "|{}|".format(data["s"])
+		if data["t"] == cc.JSON_TYPE_INT:
+			return "(int {})".format(data["v"])
+		else:
+			return None # TODO decode term
 
-    def mfa_params(self, *args):
-        """
-        Stores the entry point MFA's symbolic parameters.
-        """
-        self.params = [x["s"] for x in args]
+	# -------------------------------------------------------------------------
+	# Parse internal commands.
+	# -------------------------------------------------------------------------
 
-    def mfa_spec(self, *spec):
-        """
-        Stores the spec of the entry point MFA.
-        """
-        pass
+	def mfa_params(self, *args):
+		"""
+		Stores the entry point MFA's symbolic parameters.
+		"""
+		self.params = [x["s"] for x in args]
 
-    # -------------------------------------------------------------------------
-    # Constraints.
-    # -------------------------------------------------------------------------
+	def mfa_spec(self, *spec):
+		"""
+		Stores the spec of the entry point MFA.
+		"""
+		pass
 
-    def match_equal(self, term1, term2):
-        """
-        Asserts the predicate: term1 == term2
-        """
-        self.axs.append("(assert (= {} {}))".format(term1, term2))
+	# -------------------------------------------------------------------------
+	# Constraints.
+	# -------------------------------------------------------------------------
 
-    def match_not_equal(self, term1, term2):
-        """
-        Asserts the predicate: term1 != term2
-        """
-        self.axs.append("(assert (not (= {} {})))".format(term1, term2))
+	def guard_true(self, term):
+		"""
+		Asserts the predicate: term == true
+		"""
+		self.axioms.append("(assert (= {} (bool true)))".format(t))
 
-    def list_not_lst(self, term):
-        """
-        Asserts that: term is not list.
-        """
-        print "list_not_lst called\n" # TODO
+	def guard_false(self, term):
+		"""
+		Asserts the predicate: term == false
+		"""
+		t = self.decode(term)
+		self.axioms.append("(assert (= {} (bool false)))".format(t))
 
-    # -------------------------------------------------------------------------
-    # Reversed constraints.
-    # -------------------------------------------------------------------------
+	def match_equal(self, term1, term2):
+		"""
+		Asserts the predicate: term1 == term2
+		"""
+		t1 = self.decode(term1)
+		t2 = self.decode(term2)
+		self.axioms.append("(assert (= {} {}))".format(t1, t2))
 
-    def match_equal_reversed(self, term1, term2):
-        """
-        Asserts the predicate: Not (term1 == term2)
-        """
-        self.match_not_equal(term1, term2)
+	def match_not_equal(self, term1, term2):
+		"""
+		Asserts the predicate: term1 != term2
+		"""
+		t1 = self.decode(term1);
+		t2 = self.decode(term2);
+		self.axioms.append("(assert (not (= {} {})))".format(t1, t2))
 
-    def match_not_equal_reversed(self, term1, term2):
-        """
-        Asserts the predicate: Not (term1 != term2)
-        """
-        self.match_equal(term1, term2)
+	def list_not_lst(self, term):
+		"""
+		Asserts that: term is not list.
+		"""
+		t = self.decode(term)
+		self.axioms.append("(assert (not (is-list {})))".format(t))
 
-    def list_not_lst_reversed(self, term):
-        """
-        Asserts that: Not (term is not list).
-        """
-        print "list_not_lst_reversed called\n" # TODO
+	# -------------------------------------------------------------------------
+	# Reversed constraints.
+	# -------------------------------------------------------------------------
+
+	def guard_true_reversed(self, term):
+		"""
+		Asserts the predicate: Not (term == true)
+		"""
+		self.guard_false(term)
+
+	def guard_false_reversed(self, term):
+		"""
+		Asserts the predicate: Not (term == false)
+		"""
+		self.guard_true(term)
+
+	def match_equal_reversed(self, term1, term2):
+		"""
+		Asserts the predicate: Not (term1 == term2)
+		"""
+		self.match_not_equal(term1, term2)
+
+	def match_not_equal_reversed(self, term1, term2):
+		"""
+		Asserts the predicate: Not (term1 != term2)
+		"""
+		self.match_equal(term1, term2)
+
+	def list_not_lst_reversed(self, term):
+		"""
+		Asserts that: Not (term is not list).
+		"""
+		t = self.decode(term)
+		self.axioms.append("(assert (is-list {}))".format(t))
+
+	# -------------------------------------------------------------------------
+	# Erlang BIFs or MFAs treated as BIFs.
+	# -------------------------------------------------------------------------
+
+	def is_integer(self, term1, term2):
+		"""
+		Asserts that: term1 == is_integer(term2).
+		"""
+		t1 = self.decode(term1)
+		t2 = self.decode(term2)
+		self.axioms.append("(assert (= {} (bool (is-int {}))))".format(t1, t2))
