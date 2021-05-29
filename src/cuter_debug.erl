d9fd3b7ba6d5312727296cb8ea9967f897168bd9
Aggelos Giantsios <aggelgian@gmail.com>
2021-05-29 03:05:05 +0300

    Add a debug function to print the AST of a module [skip CI]

--- src/cuter_debug.erl
+++ src/cuter_debug.erl
@@ -1,7 +1,7 @@
 %% -*- erlang-indent-level: 2 -*-
 %%------------------------------------------------------------------------------
 -module(cuter_debug).
--export([parse_module_specs/2]).
+-export([parse_module_specs/2, parse_module/2]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parses the types & specs of a module.
@@ -28,4 +28,14 @@ get_module_attrs(Module, WithPmatch) ->
 compile_options(true) -> [to_core, {core_transform, cerl_pmatch}];
 compile_options(false) -> [to_core].
 
-
+%% Prints the AST of a module.
+%% Run as:
+%%   erl -noshell -pa ebin/ -eval "cuter_debug:parse_module(lists)" -s init stop
+-spec parse_module(module(), boolean()) -> ok.
+parse_module(M, WithPmatch) ->
+	case cuter_cerl:get_core(M, WithPmatch) of
+    {error, E} ->
+      io:format("ERROR: ~p~n", [E]);
+    {ok, AST} ->
+      io:format("~p~n", [AST])
+  end.
